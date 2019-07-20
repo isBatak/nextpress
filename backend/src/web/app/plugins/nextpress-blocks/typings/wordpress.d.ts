ab516098c0a4d20e4e2f3df2d93d9bb5bd719c18
Ivica Batinic <ivica.batinic@infinum.hr>
2019-07-20 15:23:11 +0200

    Move descriptions to types

--- backend/src/web/app/plugins/nextpress-blocks/typings/wordpress.d.ts
+++ backend/src/web/app/plugins/nextpress-blocks/typings/wordpress.d.ts
@@ -3,17 +3,59 @@ interface WPI18n {
 }
 
 interface WPBlockSettings {
+  /**
+   * Block title
+   */
   title: string;
+  /**
+   * Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+   */
   icon: string;
+  /**
+   * Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+   */
   category: string;
   keywords: Array<string>;
+
+  /**
+   * The edit function describes the structure of your block in the context of the editor.
+   * This represents what the editor will render when the block is used.
+   *
+   * The "edit" property must be a valid function.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+   */
   edit(props: {}): React.ReactNode;
+
+  /**
+   * The save function defines the way in which the different attributes should be combined
+   * into the final markup, which is then serialized by Gutenberg into post_content.
+   *
+   * The "save" property must be specified and must be a valid function.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+   */
   save(props: {}): React.ReactNode;
 }
 
 interface WPBlock {}
 
 interface WPBlocks {
+  /**
+   * Register: a Gutenberg Block.
+   *
+   * Registers a new block provided a unique name and an object defining its
+   * behavior. Once registered, the block is made editor as an option to any
+   * editor interface where blocks are implemented.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/block-api/
+   * @param  {string}   name     Block name. Block names must be string that
+   *                             contains a namespace prefix.
+   *                             Example: my-plugin/my-custom-block.
+   * @param  {Object}   settings Block settings.
+   * @return {?WPBlock}          The block, if it has been successfully
+   *                             registered; otherwise `undefined`.
+   */
   registerBlockType(name: string, settings: WPBlockSettings): WPBlock | undefined;
 }
 
